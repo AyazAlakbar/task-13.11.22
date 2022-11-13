8081815aa6439e24034d45c793bd61f027c156ca
alexberg90 <ayazya@code.edu.az>
2022-11-13 14:55:54 +0400

    13.12.11task

---
+++ 13.11.22 tasks/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+
+namespace _13._11._22_tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+
+            // Console.WriteLine(ReverseName("Ayaz Alakbarov"));
+
+
+           // Console.WriteLine(Replace("A,c,a,d,e,m,y"));
+        
+        }
+
+
+
+
+
+        //task--2den cox sozu ters cevir
+        static string ReverseName(string word)
+        {
+
+            string str = word;
+
+            string res = string.Join(" ", str.Split(' ').Select(str => new String(str.Reverse().ToArray())));
+
+            return res;
+        }
+
+
+
+
+
+        static string Replace(string str)
+        {
+
+            foreach (var item in str)
+            {
+                str = str.Replace(",", "");
+            }
+            return str;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
